22d1b1e843c088dddc8848ae4e8f85f97a53f2be
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-04-09 23:31:23 -0400

    Convert LoginForm to TS, and refactor images to /assets

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,55 +1,47 @@
 import React, { useState } from "react";
-import logo from "../images/mcgill_logo.jpg"; 
+import logo from "../assets/images/mcgill_logo.jpg";
+
+// Specify the structure of the props
+interface Props {
+  Login: Function;
+  error: string;
+}
 
 // Login function and error value passed from App.js
-function LoginForm({ Login, error }) {
+function LoginForm({ Login, error }: Props) {
   const [details, setDetails] = useState({ email: "", password: "" });
 
   // on submit pass email and password values entered by user
-  const submitHandler = (e) => {
+  function submitHandler(e: React.FormEvent<HTMLFormElement>): void {
     e.preventDefault();
     Login(details);
-  };
+  }
 
   // displays error if not null (Note: if error != null then error has occured)
   // returns form with username and password input feilds
   return (
     <form onSubmit={submitHandler}>
       <div className="form-inner">
-        <img className="logo" src={logo} />
+        <img className="logo" src={logo} alt="mcgill-logo" />
 
         <p className="top">Sign in with your username and password.</p>
         {error !== "" ? <div className="error"> * {error} </div> : ""}
 
         <div className="form-group">
-          <input
-            type="email"
-            name="email"
-            placeholder="Username"
-            id="email"
-            onChange={(e) => setDetails({ ...details, email: e.target.value })}
-            value={details.email}
-          />
+          <input type="email" name="email" placeholder="Username" id="email" onChange={(e) => setDetails({ ...details, email: e.target.value })} value={details.email} />
         </div>
 
         <div className="form-group">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            id="password"
-            onChange={(e) =>
-              setDetails({ ...details, password: e.target.value })
-            }
-            value={details.password}
-          />
+          <input type="password" name="password" placeholder="Password" id="password" onChange={(e) => setDetails({ ...details, password: e.target.value })} value={details.password} />
         </div>
 
         <div className="sign-in-button">
           <input type="submit" value="Sign in" />
         </div>
 
-        <p className="bottom"><span className="links">Forget password</span> or <span className="links">Register</span></p>
+        <p className="bottom">
+          <span className="links">Forget password</span> or <span className="links">Register</span>
+        </p>
       </div>
     </form>
   );
