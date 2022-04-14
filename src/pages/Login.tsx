e7b05a7449d1e783d37286026a31b11670d29a77
madisonkruh <madisonkruh@gmail.com>
2022-04-14 18:21:59 -0400

    change path

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/mcgill_logo.jpg";
-import "../login.css";
+import "./login.css";
 import { UserContext } from "../App";
 import { User } from "../classes/User";
 import { UserClass } from "../types/userType";
