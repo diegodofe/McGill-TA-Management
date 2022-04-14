6bb264b190685512264bb3b1a36870526daf8977
madisonkruh <madisonkruh@gmail.com>
2022-04-14 13:40:29 -0400

    Create top bar and styling

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./topbar.css";
+import logout from "../../assets/images/icons/logout.png";
+import rocket from "../../assets/images/icons/rocket.png";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+        <img className="rocket" src={rocket} alt="rocket" />
+        </div>
+        <div className="topRight">
+          <img className="logout" src={logout} alt="logout" />
+        </div>
+      </div>
+    </div>
+  );
+}
