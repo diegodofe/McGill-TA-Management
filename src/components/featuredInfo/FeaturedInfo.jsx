56493ae0e30929cbedebe46f94454f21bce9435c
madisonkruh <madisonkruh@gmail.com>
2022-04-14 16:06:02 -0400

    Added material ui table

--- src/components/featuredInfo/FeaturedInfo.jsx
+++
@@ -1,9 +0,0 @@
-import "./featuredInfo.css";
-
-export default function FeaturedInfo() {
-  return (
-    <div className="featured">
-      <div className="featuredItem"></div>
-    </div>
-  );
-}
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,12 +3,13 @@ import "./topbar.css";
 import logout from "../../assets/images/icons/logout.png";
 import rocket from "../../assets/images/icons/rocket.png";
 
-export default function Topbar() {
+export default function Topbar({ title })  {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
         <img className="rocket" src={rocket} alt="rocket" />
+        {title} 
         </div>
         <div className="topRight">
           <img className="logout" src={logout} alt="logout" />
