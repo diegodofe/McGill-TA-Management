ff408a9bcb81774e0f0c387b1d7fa30bed7c4d4f
madisonkruh <madisonkruh@gmail.com>
2022-04-14 15:02:21 -0400

    Added all tabs to sidebar but only some should be displayed depending on user type

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import "./sidebar.css";
-import { HomeOutlined } from "@mui/icons-material";
+import { HomeOutlined, GroupOutlined, ClassOutlined } from "@mui/icons-material";
+import logo from "../../assets/images/mcgill_logo_transp.png";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Dashboard</h3>
+          <img className="logo" src={logo} alt="logo" />
+
           <ul className="sidebarList">
             <li className="sidebarListItem">
               <HomeOutlined />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              {/* only for professors and TAs */}
+              <GroupOutlined />
+              TA Management
+            </li>
+            <li className="sidebarListItem">
+              {/* only for sysop */}
+              <GroupOutlined />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              {/* only for sysop */}
+              <ClassOutlined />
+              Courses
             </li>
           </ul>
         </div>
