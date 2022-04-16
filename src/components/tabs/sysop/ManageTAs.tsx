147bd874e9835170f500371211202fcb22e74415
AlexFerence <alexference23@gmail.com>
2022-04-16 15:20:46 -0400

    ManageTaTabAgain

--- src/components/tabs/sysop/ManageTAs.tsx
+++ src/components/tabs/sysop/ManageTAs.tsx
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
-
-
+import React, { useEffect, useState } from 'react';
 const ManageTAs = () => {
 
     const [tas, setTas] = useState([]);
 
     const fetchTAData = async () => {
         try {
-
+            const res = await fetch('https://winter2022-comp307-group8.cs.mcgill.ca/ta/all');
+            const data = await res.json();
+            setTas(data.tas);
         }
         catch (err) {
             console.error(err);
         }
     }
+
+    useEffect(() => {
+        fetchTAData();
+    }, []);
+
     return (
         <div>
-            Testing
             <table>
                 <thead>
                     <tr>
+                        <th>Email</th>
                         <th>First name</th>
                         <th>Last name</th>
-                        <th>Email</th>
+                        <th>Faculty</th>
+                        <th>Department</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    {tas.map((ta, i) => (
+                        <div>
+                            {ta.email}
+                        </div>
+                    ))}
                 </tbody>
             </table>
         </div>
