57a35b95c9e22c2a1a2f540b567cd9fb5548dfd5
madisonkruh <madisonkruh@gmail.com>
2022-04-17 13:53:20 -0400

    Manage Ta Admin table styling

--- src/components/tabs/sysop/ManageTAAdmins.tsx
+++ src/components/tabs/sysop/ManageTAAdmins.tsx
@@ -1,50 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import AddTaAdminForm from './AddTaAdminForm';
-import TAAdminRow from './TAAdminRow';
+import React, { useEffect, useState } from "react";
+import AddTaAdminForm from "./AddTaAdminForm";
+import TAAdminRow from "./TAAdminRow";
+import "../../../style/userTable.css";
 
 const ManageTAAdmins = () => {
+  const [taAdmins, setTAAdmins] = useState([]);
 
-    const [taAdmins, setTAAdmins] = useState([])
-
-    const handleFetchTAAdmins = async () => {
-        try {
-            const res = await fetch("https://winter2022-comp307-group8.cs.mcgill.ca/taAdmin/all");
-            const data = await res.json();
-            console.log("taAdmins loaded");
-            setTAAdmins(data.tas);
-        } catch (err) {
-            console.error(err);
-        }
+  const handleFetchTAAdmins = async () => {
+    try {
+      const res = await fetch(
+        "https://winter2022-comp307-group8.cs.mcgill.ca/taAdmin/all"
+      );
+      const data = await res.json();
+      console.log("taAdmins loaded");
+      setTAAdmins(data.tas);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    useEffect(() => {
-        console.log("ManageTAAdmins useEffect");
-        handleFetchTAAdmins();
-    }, [])
+  useEffect(() => {
+    console.log("ManageTAAdmins useEffect");
+    handleFetchTAAdmins();
+  }, []);
 
-
-    return (
-        <div>
-            {/* Table that lists all the TAAdmins */}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Faculty</th>
-                        <th>Department</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {taAdmins.map((taAdmin, i) => (
-                        <TAAdminRow key={i} taAdmin={taAdmin} handleFetchTAAdmins={handleFetchTAAdmins} />
-                    ))}
-                </tbody>
-            </table>
-            <AddTaAdminForm handleFetchTAAdmins={handleFetchTAAdmins} />
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* Table that lists all the TAAdmins */}
+      <div id="profTable">
+        <table>
+          <thead>
+            <tr>
+              <th className="column0"></th>
+              <th className="column1">Email</th>
+              <th className="column2">First name</th>
+              <th className="column3">Last name</th>
+              <th className="column4">Faculty</th>
+              <th className="column5">Department</th>
+            </tr>
+          </thead>
+          <tbody>
+            {taAdmins.map((taAdmin, i) => (
+              <TAAdminRow
+                key={i}
+                taAdmin={taAdmin}
+                handleFetchTAAdmins={handleFetchTAAdmins}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <AddTaAdminForm handleFetchTAAdmins={handleFetchTAAdmins} />
+    </div>
+  );
+};
 
 export default ManageTAAdmins;
