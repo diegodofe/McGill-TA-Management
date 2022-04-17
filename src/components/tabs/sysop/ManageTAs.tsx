8742d9c9bc25922f423a09e5837192c24a11d51c
madisonkruh <madisonkruh@gmail.com>
2022-04-17 14:15:30 -0400

    Created seperate file for TA rows

--- src/components/tabs/sysop/ManageTAs.tsx
+++ src/components/tabs/sysop/ManageTAs.tsx
@@ -1,53 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import AddTAForm from './AddTAForm';
+import React, { useEffect, useState } from "react";
+import AddTAForm from "./AddTAForm";
+import TARow from "./TARow";
+import "../../../style/userTable.css";
+
 const ManageTAs = () => {
+  const [tas, setTas] = useState([]);
 
-    const [tas, setTas] = useState([]);
-
-    const fetchTAData = async () => {
-        try {
-            const res = await fetch('https://winter2022-comp307-group8.cs.mcgill.ca/ta/all');
-            const data = await res.json();
-            setTas(data.tas);
-        }
-        catch (err) {
-            console.error(err);
-        }
+  const fetchTAData = async () => {
+    try {
+      const res = await fetch(
+        "https://winter2022-comp307-group8.cs.mcgill.ca/ta/all"
+      );
+      const data = await res.json();
+      setTas(data.tas);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    useEffect(() => {
-        fetchTAData();
-    }, []);
+  useEffect(() => {
+    fetchTAData();
+  }, []);
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Faculty</th>
-                        <th>Department</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tas.map((ta, i) => (
-                        <tr key={i}>
-                            <td>{ta.email}</td>
-                            <td>{ta.firstName || ta.backupFirstName}</td>
-                            <td>{ta.lastName || ta.backupLastName}</td>
-                            <td>{ta.faculty}</td>
-                            <td>{ta.department}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            {/* Div with height 20 */}
-            <div style={{ height: "20px" }}></div>
-            <AddTAForm fetchTAData={fetchTAData} />
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* Table that lists all the TAAdmins */}
+      <div id="profTable">
+        <table>
+          <thead>
+            <tr>
+              <th className="column0"></th>
+              <th className="column1">Email</th>
+              <th className="column2">First name</th>
+              <th className="column3">Last name</th>
+              <th className="column4">Faculty</th>
+              <th className="column5">Department</th>
+            </tr>
+          </thead>
+          <tbody>
+          {tas.map((ta, i) => (
+              <TARow
+                key={i}
+                ta={ta}
+                fetchTAData={fetchTAData}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <AddTAForm fetchTAData={fetchTAData} />
+    </div>
+  );
+};
 
 export default ManageTAs;
