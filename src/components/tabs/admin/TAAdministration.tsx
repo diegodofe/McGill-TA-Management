7d0428dce02a783ff5425146eac57b7b176d1e67
madisonkruh <madisonkruh@gmail.com>
2022-04-17 22:35:28 -0400

    fixed weird tabs

--- src/components/tabs/admin/TAAdministration.tsx
+++ src/components/tabs/admin/TAAdministration.tsx
@@ -59,6 +59,7 @@ const TAAdministration = () => {
       <div className="inline">
         <h2 className="inline course-name">{`${currentCourse.courseNumber}: ${currentCourse.courseName}`}</h2>
         <ViewTAWishlist course={allCoursesAtMcGill[2]} isProfessor={false} />
+        <br />
         <Tabs defaultActiveKey="0" transition={false} id="noanim-tab" className="sub">
           <Tab
             className="sub"
