5bce7c68d66cc394e3663da3e788460d33af932b
Diego Dorantes-Ferreira <63019625+diegodofe@users.noreply.github.com>
2022-04-17 23:39:32 -0400

    Diego/logout (#48)
    * diego/Logout
    
    * Logout icon
    
    Co-authored-by: madisonkruh <madisonkruh@gmail.com>

---
+++ src/pages/LoggedOut.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/mcgill_logo.jpg";
+
+const LoggedOut: React.FC = () => {
+  return (
+    <div className="welcome">
+      <form>
+        <div className="form-inner">
+          <img className="logo" src={logo} alt="mcgill-logo" />
+          <h2 style={{ marginTop: "40px", marginBottom: "5px" }}>Sign out</h2>
+          <p>You have successfully signed out.</p>
+          <p className="bottom">
+            <Link className="links" to="/login">
+              Return to Login
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoggedOut;
