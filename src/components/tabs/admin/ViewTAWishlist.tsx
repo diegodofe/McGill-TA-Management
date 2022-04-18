a1422091f087f7e2cf550dcbf73ac7cf92425646
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-04-17 20:41:22 -0400

    Create basic structure for add TA form

--- src/components/tabs/admin/ViewTAWishlist.tsx
+++ src/components/tabs/admin/ViewTAWishlist.tsx
@@ -4,21 +4,26 @@ import { OverlayTrigger, Popover } from "react-bootstrap";
 import "../../../style/userTable.css";
 import Course from "../../../classes/Course";
 import { TA } from "../../../classes/TA";
+import ProfWishlistForm from "../professor/ProfWishlistForm";
 
 function ViewTAWishlist({ course }: { course: Course }) {
   return (
     <div className="view-wishlist" id="ta-wishlist-modal">
       {/** Pop up window*/}
       <OverlayTrigger
-        trigger="focus"
+        trigger="click"
         placement="top"
         overlay={
           <Popover id={"wishlist-popover"}>
             <Popover.Header as="h3">{"Professor's Wishlist"}</Popover.Header>
             <Popover.Body>
+              {/** Render all the TA names on this course list */}
               {course.wishlist.map((ta: TA, i: number) => (
                 <h6 key={i}>{`${ta.firstName} ${ta.lastName}`}</h6>
               ))}
+
+              {/** Create the add TA to wishlist form */}
+              <ProfWishlistForm />
             </Popover.Body>
           </Popover>
         }
